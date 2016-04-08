9b054031448269b5e8fb16ed7a9e998c18f627a4
keke <Value.Pu@zhan.com>
2016-04-08 09:55:34 +0800

    代码提交

--- app/src/main/java/com/hardware/api/ApiConstants.java
+++ app/src/main/java/com/hardware/api/ApiConstants.java
@@ -6,5 +6,5 @@ package com.hardware.api;
 public class ApiConstants {
     public static final String BASE_URL = "http://121.43.111.133:50013/";
 
-    public static final String MOBILE_HOME_PRODUCTS_LIST = BASE_URL+"common/aplHome/mobilehomeproductsList";
+    public static final String MOBILE_HOME_PRODUCTS_LIST ="common/aplHome/mobilehomeproductsList";
 }
